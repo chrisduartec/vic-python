d86ae0125436dbf986884a9544c5c2039b2d2018
chrisduartec <chris.duartec@gmail.com>
2023-10-16 22:14:38 -0300

    Create function getProductionMethods

--- vic.py
+++ vic.py
@@ -5,8 +5,6 @@ import os
 # Describe path
 path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\Victoria 3\\game\\common\\production_methods\\"
 
-
-
 # Create a list of file names
 file_names = []
 
@@ -21,7 +19,11 @@ with os.scandir(path) as entries: # Scan through the names within the path
 
 
 # Create a function 
-def getProductionMethods():
+def getProductionMethods(file_name):
+
+    # Define the file name
+    file_to_open = path + file_name
+
     # Create a list of rows
     rows = []
 
@@ -29,7 +31,7 @@ def getProductionMethods():
     columns = ["pm", "trade_good", "value"]
 
     # Import the text file with the encoding
-    with open(path + '01_industry.txt', 'r', encoding = 'utf-8-sig') as file:
+    with open(file_to_open, 'r', encoding = 'utf-8-sig') as file:
         content = file.readlines()
 
     # Define the pattern
@@ -71,63 +73,12 @@ def getProductionMethods():
         else:
             continue
 
+    # Create data frame
+    df = pd.DataFrame(rows, columns = columns)
 
+    # Return data frame
+    return(df)
 
-# Create a list of rows
-rows = []
-
-# Define the column names
-columns = ["pm", "trade_good", "value"]
-
-# Import the text file with the encoding
-with open(path + '01_industry.txt', 'r', encoding = 'utf-8-sig') as file:
-    content = file.readlines()
-
-# Define the pattern
-pattern = f"\d+"
-
-for line in content:
-
-    # Check if the line starts with 'pm_' or 'building'
-    if any(keyword in line for keyword in ('pm_', 'building_')):
-
-        # Split on equal sign
-        line_strip = line.strip()
-        key = line_strip.split("=")[0]
-        
-        # Get the pm
-        if key.startswith("pm_"):
-            pm = key.strip()[3:]
-        
-        # Get input/output/employment and the trade_good
-        elif key.startswith("building_"):
-            if "modifiers" not in key.split("_")[1]:
-                io = key.split("_")[1]
-                trade_good = key.split("_")[2]
-
-                # Get the values associated with the goods
-                value_string = line_strip.split("=")[1]
-                value_init = int(re.search(pattern, value_string).group())
-                
-                # If it is an input, change value to negative
-                if io == 'input':
-                    value = int((-1)*value_init)
-                
-                else:
-                    value = value_init
-
-                # Append into row
-                rows.append([pm, trade_good, value])
-
-    else:
-        continue
-
-# Create data frame
-df = pd.DataFrame(rows, columns = columns)
-
-
-
-
-
-
+df_industry = getProductionMethods(file_names[1])
+df_industry
 
